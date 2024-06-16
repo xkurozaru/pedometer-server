4b5dbea919e4f05d991da262fae395694c640aaf
xkurozaru <kurozaru.210@gmail.com>
2024-06-16 22:41:20 +0900

    feat: ✨ 認証後のリダイレクト先を追加してみる

--- api/auth/index.go
+++ api/auth/index.go
@@ -10,6 +10,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	registry := registry.NewRegistry()
 
 	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("<h1>登録が完了しました</h1>"))
 	case http.MethodPost:
 		registry.NewAuthHandler().PostAuth(w, r)
 	case http.MethodOptions:
