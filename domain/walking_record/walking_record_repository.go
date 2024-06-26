5ab7b2b1c143d442e63cf50d19c9a4cd8235342e
xkurozaru <kurozaru.210@gmail.com>
2024-06-26 16:54:39 +0900

    style: 💄 メソッドのリネーム

--- domain/walking_record/walking_record_repository.go
+++ domain/walking_record/walking_record_repository.go
@@ -1,7 +1,6 @@
 package walking_record
 
 type WalkingRecordRepository interface {
-	Upsert(w WalkingRecord) error
-	AllUpsert(ws WalkingRecords) error
+	UpsertAll(ws WalkingRecords) error
 	FindByFilter(filter WalkingRecordFilter, order WalkingRecordOrder) (WalkingRecords, error)
 }
